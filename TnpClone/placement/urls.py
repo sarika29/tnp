36a92bf08806a68692e6d5f944551d76d4ac3125
dsouzajess98 <djessica@student.nitw.ac.in>
2018-09-02 04:26:19 +0530

    commit

--- TnpClone/placement/urls.py
+++ TnpClone/placement/urls.py
@@ -6,8 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = [
 	url(r'^signin/$',views.signup,name="index"),
 	url(r'^profile/$', views.profile, name='profile'),
-	url(r'^index/$', views.main, name='profile'),
-	url(r'^upcompany/$', views.upcompany, name='profile')
+	url(r'^index/$', views.main, name='main'),
+	url(r'^upcompany/$', views.upcompany, name='upcompany'),
+	url(r'^login/$', views.signin, name='login')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- TnpClone/placement/views.py
+++ TnpClone/placement/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from .models import *
 
@@ -45,7 +45,7 @@ def main(request):
 	response={}
 	current_user = request.user.username
 	response['name']=current_user
-	obj=Student.objects.get(regno= 123)
+	obj=Student.objects.get(regno= current_user)
 	response['student']=obj
 	response['file']=obj.resume
 	return render(request,'production/index.html',response)
@@ -54,4 +54,22 @@ def upcompany(request):
 	response={}
 	current_user = request.user.username
 	response['name']=current_user
+	std=Student.objects.get(username=current_user)
+	obj=Company.objects.filter(min_cgpa__lte=std.cgpa)
+	response['company']=obj
 	return render(request,'production/upcompany.html',response)
+
+def signin(request):
+	response = {}
+	if request.method == 'POST' :
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is None :
+			return render(request,'login.html',response)
+		else :
+			login(request,user)
+			return redirect('/index')
+	return render(request,'login.html',response)
+
+
