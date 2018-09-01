abef47d9816f2a6df7a39e94cf9be36055787a08
sarika29 <sarika292820@gmail.com>
2018-09-02 03:48:25 +0530

    commit

--- TnpClone/placement/urls.py
+++ TnpClone/placement/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
 	url(r'^index/$', views.main, name='profile'),
 	url(r'^upcompany/$', views.upcompany, name='profile')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
