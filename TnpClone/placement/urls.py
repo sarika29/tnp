66163ab63e81fdf964e2971063ca614f04a1fd04
dsouzajess98 <djessica@student.nitw.ac.in>
2018-09-02 00:24:01 +0530

    added basic login page

--- TnpClone/placement/urls.py
+++ TnpClone/placement/urls.py
@@ -3,4 +3,6 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
+
+url(r'^signin/$',views.signin,name="index"),
 ]
--- TnpClone/placement/views.py
+++ TnpClone/placement/views.py
@@ -4,3 +4,17 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def signin(request):
+	response = {}
+	if request.method == 'POST' :
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is None :
+			return render(request,'login.html',response)
+		else :
+			login(request,user)
+			return redirect('/index')
+	return render(request,'login.html',response)
